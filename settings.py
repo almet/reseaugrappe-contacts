bfc1326d5d74481710231e8c20cde2fe390125bf
Alexis Metaireau <alexis@notmyidea.org>
2012-07-06 17:13:19 +0200

    change the grappelli url

--- settings.py
+++ settings.py
@@ -61,7 +61,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+ADMIN_MEDIA_PREFIX = "grappelli/"
 
 # Additional locations of static files
 STATICFILES_DIRS = (
